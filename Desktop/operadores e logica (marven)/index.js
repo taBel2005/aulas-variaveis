f14466e9c713a0f043f8e00c140bde6bf20912f4
marvencbss <marvencbs12@gmail.com>
2023-04-26 20:46:40 -0300

    operadores e logica (marven)

---
+++ Desktop/operadores e logica (marven)/index.js
@@ -0,0 +1,49 @@
+//operadores e logica de programação
+
+//exercicio 1:
+// a. false (pois bool2=false)
+// b. false (pois bool2=false)
+// c. true (pois (bool1 || bool2 = false, então !(bool1 || bool2) = !false = true)
+// d. boolean
+
+//exercicio 2
+//será impresso a junção de ambos os números 
+
+//exercicio 3
+//erro pois faltou ao código definir seu tipo (number) antes de prompt
+
+
+
+
+//exercicios de escrita de codigo
+
+// exercicio 1
+
+// const idd = prompt("Digite sua idade.")
+// const iddAmigo = prompt("Digite a idade do seu melhor amigo.")
+
+// console.log("Sua idade é maior do que a do seu amigo?" ,idd > iddAmigo)
+// console.log("A diferença de idade entre vocês é" ,idd - iddAmigo)
+
+// exercicio 2
+
+// const nmrPar = prompt("Digite um número par")
+// console.log(nmrPar % 2);
+// c) sim, sempre resultará 0
+// d) resultará sempre em 1
+
+//exercicio 3
+
+// const idd = prompt("Digite sua idade, em anos.")
+// console.log("Sua idade em meses é" ,idd * 12);
+// console.log("Sua idade em dias é" ,idd * 365)
+// console.log("Sua idade em horas é" ,idd * 8640)
+
+//exercicio 4 
+
+// const nmrUm = Number (prompt("Digite um número."))
+// const nmrDois = Number (prompt("Digite outro número"))
+// console.log("O primeiro número é maior que o segundo" ,nmrUm > nmrDois);
+// console.log("O primeiro número é igual ao segundo" ,nmrUm === nmrDois)
+// console.log("O primeiro número é divisível pelo segundo" ,(nmrUm % nmrDois) == 0)
+// console.log("O segundo número é divisível pelo primeiro" ,(nmrDois % nmrUm) == 0)
