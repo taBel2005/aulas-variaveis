a32ff10f50b0e803f909a5b08b2d59ff7feb9f05
marvencbss <marvencbs12@gmail.com>
2023-04-27 21:31:34 -0300

    strings-e-array

---
+++ Desktop/stringsarray marven/index.js
@@ -0,0 +1,36 @@
+// // // const nome = prompt("digite seu nome")
+// // // const corFav = prompt("digite sua cor favorita")
+// // // console.log("A cor favorita do" ,nome,"é" ,corFav, )
+
+// // // const nome = prompt("digite seu nome")
+// // // const corFav = prompt("digite sua cor favorita")
+// // // console.log(`A cor favorita do ${nome} é ${corFav}`);
+
+// // // const nome = "jose"
+// // // console.log(nome.length);
+
+// // // const sobrenome = "Zuckenberg"
+// // // console.log(sobrenome.toLowerCase());
+// // // console.log(sobrenome.toUpperCase());
+
+// // const nome = "          maria"
+// // console.log(nome.trim());
+
+// const receita = "bolo de cenoura"
+// console.log(receita.includes("cenoura")); //true
+// console.log(receita.includes("abacaxi")); //false
+
+// receita = "bolo de CENOURA".toLowerCase()
+// console.log(receita.includes("cenoura"));
+
+// receita = "   bolo de CENOURA".toLowerCase()
+// console.log(receita.trim().toLowerCase().includes("cenoura"));
+
+// const receita = "   bolo de abacaxi"
+// const novaReceita =receita.trim().replace("abacaxi","cenoura")
+// console.log(novaReceita);
+
+// const pergunta = prompt("Escreva uma frase")
+// console.log(pergunta.toUpperCase());
+// console.log(pergunta.replaceAll("o","i"))
+// console.log(pergunta.length)
